2664a8e3cb87fc99646b9b09b2462fe868ffe396
Rohan Bohra <rohanbohra8@gmail.com>
2024-06-01 22:50:15 +0530

    project structure defined

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -4,15 +4,28 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    resolve: { //we added this
+      alias: { 
+        '@lib': resolve('src/main/lib'),
+        '@shared': resolve('src/shared'),
+      }
+    } //yaha tak
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    assetsInclude: 'src/renderer/assets/**', // we added this, to include all the assests from the location we gave 
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@shared': resolve('src/shared'), // we added this 
+        '@/hooks': resolve('src/renderer/src/hooks'),
+        '@/assets': resolve('src/renderer/src/assets'),
+        '@/store': resolve('src/renderer/src/store'),
+        '@/components': resolve('src/renderer/src/components'),
+        '@/mocks': resolve('src/renderer/src/mocks') //yaha tak add kiya 
       }
     },
     plugins: [react()]
